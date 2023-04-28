843ae4fe49139b6fd35fa61a0f384c32e98ade02
RuanTurbo <126512315+RuanTurbo@users.noreply.github.com>
2023-04-28 15:48:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,105 @@
-canvas = document.getElementById('myCanvas'); 
+
+canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
+
 nasaMarsImagesArray = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"];
+
 randomNumber = Math.floor(Math.random() * 4);
 console.log(randomNumber);
-roverWidth = 100; 
+roverWidth = 100;
 roverHeight = 90;
-backgroundImage = "mars.jpg";
-roverImage = "rover.png";
- roverX = 10; roverY = 10;
- function add() 
- {
-     backgroundImgTag = new Image();//definindo uma variável com uma nova imagem
-       backgroundImgTag.onload = uploadBackground;// ajustando uma função, ao carregar essa variável
-         backgroundImgTag.src = backgroundImage; // carregar a imagem 
-         roverImgTag = new Image(); //definindo uma variável com uma nova imagem 
-         roverImgTag.onload = uploadrover; // ajustando uma função, ao carregar essa variável 
-         roverImgTag.src = roverImage; 
-         // carregar a imagem 
-        }
-        function uploadBackground()
-         {
-             ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
-             }
-              function uploadrover()
-               {
-                 ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight); 
-                }
-                function myKeyDown(e)
-                 
-                 keyPressed = e.keyCode;
-                  console.log(keyPressed);
-                   if(keyPressed == '87')
-                    {
-                     up();
-                   console.log("cima");
-                    } if(keyPressed == '83')
-                    { down();
-                     console.log("baixo");
-                    }
-                    if(keyPressed == '65')
-                    { left();
-                     console.log("esquerda"); }
-                    if(keyPressed == '68')
-                     { right();
-                     console.log("direita"); 
-                  } 
-                     function up()
-                     {
-                     if(roverY >=0)
-                      { roverY = roverY - 10;
-                     console.log("Quando direcional cima for pressionada, x = " + roverX + " | y = " +roverY);
-                      uploadBackground(); uploadrover(); } }
-                      function down()
-                       {
-                         if(roverY <=500)
-                          { roverY =roverY+ 10;
-                             console.log("Quando direcional baixo for pressionada, x = " + roverX + " | y = " +roverY);
-                              uploadBackground(); uploadrover(); 
-                            } }
-                            function left()
-                             { 
-                                if(roverX >= 0)
-                                 { roverX =roverX - 10; console.log("Quando direcional esquerda for pressionada, x = " + roverX + " | y = " +roverY);
-                                  uploadBackground(); uploadrover(); 
-                                } } 
-                                  function right() 
-                                  {
-                                     if(roverX <= 700) { roverX =roverX + 10;
-                                         console.log("Quando direcional direita for pressionada, x = " + roverX + " | y = " +roverY);
 
-                                   uploadBackground(); uploadrover(); } }
+backgroundImage = nasaMarsImagesArray[randomNumber];
+console.log("backgroundImage = " + backgroundImage);
+roverImage = "rover.png";
+
+roverX = 10;
+roverY = 10;
+
+function add() {
+	backgroundImgTag = new Image(); //definindo uma variável com uma nova imagem
+	backgroundImgTag.onload = uploadBackground; // definindo uma função, ao carregar esta variável
+	backgroundImgTag.src = backgroundImage;   // carregando a imagem
+
+	roverImgTag = new Image(); //definindo uma variável com uma nova imagem
+	roverImgTag.onload = uploadRover; // definindo uma função, ao carregar esta variável
+	roverImgTag.src = roverImage;   // carregando a imagem
+
+function uploadBackground() {
+	ctx.drawImage(backgroundImgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadRover() {
+	ctx.drawImage(roverImgTag, roverX, roverY, roverWidth, roverHeight);
+}
+
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '87')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '83')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '65')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '68')
+		{
+			right();
+			console.log("right");
+		}
+}
+
+function up()
+{
+	if(roverY >=0)
+	{
+		roverY = roverY - 10;
+		console.log("Quando a seta para cima é pressionada,  x = " + roverX + " | y = " +roverY);
+		 uploadBackground();
+		 uploadRover();
+	}
+}
+function down()
+{
+	if(roverY <=500)
+	{
+		roverY =roverY+ 10;
+		console.log("Quando a seta para baixo é pressionada,  x = " + roverX + " | y = " +roverY);
+		uploadBackground();
+		uploadRover();
+	}
+}
+function left()
+{
+	if(roverX >= 0)
+	{
+		roverX =roverX - 10;
+		console.log("Quando a seta para esquerda é pressionada,  x = " + roverX + " | y = " +roverY);
+		uploadBackground();
+		 uploadRover();
+	}
+}
+function right()
+{
+	if(roverX <= 700)
+	{
+		roverX =roverX + 10;
+		console.log("Quando a seta para direita é pressionada,  x = " + roverX + " | y = " +roverY);
+		uploadBackground();
+		uploadRover();
+   }
+}
+}
